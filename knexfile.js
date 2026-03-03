0816a7c861f025d27f413f7caf3ffb5375871cf9
saqib nihal <saqibnihal.dlktechnologies@gmial.com>
2026-03-03 17:01:02 +0530

    changes

--- knexfile.js
+++ knexfile.js
@@ -4,31 +4,31 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
-    client: 'mysql2',
+    client: "mysql2",
     connection: {
-      host:"localhost",
-      user:"root",
-      password:"root",
-      database:"dlk"
-    }
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "dlk",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "mysql2",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: "your_production_host",
+      user: "your_production_user",
+      password: "Dlk@2026#Secure",
+      database: "your_production_database",
+      ssl: false, // change to true if using cloud DB
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
