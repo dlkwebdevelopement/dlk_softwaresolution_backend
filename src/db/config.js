7d50db093c5923112fa2f62362bc594bdd1c4667
saqib nihal <saqibnihal.dlktechnologies@gmial.com>
2026-03-06 15:51:31 +0530

    db

--- src/db/config.js
+++ src/db/config.js
@@ -1,10 +1,11 @@
 const { Model } = require("objection");
 const knex = require("knex");
-const config = require("../../knexfile").development; 
-const db = knex(config); 
-
+ 
+const environment = process.env.NODE_ENV || "development";
+const config = require("../../knexfile")[environment];
+ 
+const db = knex(config);
+ 
 Model.knex(db);
-
+ 
 module.exports = db;
-
-
