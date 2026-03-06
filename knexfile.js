65d4343c11ce4a544950d61c1ee4131943455cd9
saqib nihal <saqibnihal.dlktechnologies@gmial.com>
2026-03-06 15:20:52 +0530

    again

--- knexfile.js
+++ knexfile.js
@@ -3,29 +3,42 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+// Update with your config settings.
+
 module.exports = {
   development: {
     client: "mysql2",
+
     connection: {
       host: "localhost",
+
       user: "root",
+
       password: "root",
+
       database: "dlk",
     },
   },
 
   production: {
     client: "mysql2",
+
     connection: {
       host: "localhost",
+
       user: "dlkuser",
+
       password: "Dlk@2026#Secure",
+
       database: "dlksoftware_db",
     },
+
     pool: {
       min: 2,
+
       max: 10,
     },
+
     migrations: {
       tableName: "knex_migrations",
     },
